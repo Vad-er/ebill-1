d76e443bddf7dc6fb10af23c75a0b48c45fc3919
Vad-er <930abhishek@gmail.com>
2014-08-16 21:46:51 +0530

    Create addvalues.php

---
+++ addvalues.php
@@ -0,0 +1,20 @@
+<?php
+include 'simple_connect.php';
+
+$name = $_POST['name'];                             //taking values from the form
+$city = $_POST['city'];                             //taking values from the form
+
+if(!$_POST['submit']){
+	echo "fill out the form";
+	header('Location:test2.php');
+
+}else{
+	mysql_query("INSERT INTO public (`NAME`,`CITY`)   //the database coloumns
+	VALUES('$name','$city')")or die(mysql_error());   //inputing into table
+
+	echo "user has been added";
+	header('Location:simple_connect.php');
+}
+	
+
+?>
