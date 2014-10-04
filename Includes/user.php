c572ebf9b379efdd5f7ee4a332e4f727107cf5f0
Ameen Khan <ameenkhan07@gmail.com>
2014-10-04 16:16:30 +0530

    User database functions file added

---
+++ Includes/user.php
@@ -0,0 +1,31 @@
+<?php 
+    
+
+    function retrieve_complaints ($id) {
+        include("config.php");
+        $query = "SELECT * FROM complaint where uid={$id} ";
+        $query .= "ORDER BY id DESC";
+        $result1 = mysqli_query($con,$query);
+        return $result1;
+    }
+
+    function retrieve_bills_history($id) {
+        include("config.php");
+        $query = "SELECT * FROM bill where uid={$id} ";
+        $query .= "ORDER BY id DESC";
+        $result = mysqli_query($con,$query);
+        return $result;
+    }
+
+    function retrieve_bills_due($id) {
+        include("config.php");
+        $query  = "SELECT bill.bdate AS bdate, bill.units AS units, bill.ddate AS ddate, transaction.payable AS payable, ";
+        $query .= " bill.amount AS amount ,transaction.payable-bill.amount AS dues ";
+        $query .= "FROM bill , transaction ";
+        $query .= "WHERE transaction.bid=bill.id AND bill.uid={$id} AND bill.status='PENDING' ";
+        $query .= "ORDER BY ddate desc"; 
+        $result = mysqli_query($con,$query);
+        return $result;
+    }
+
+ ?>
