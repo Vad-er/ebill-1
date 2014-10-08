18a76be0f8414b855ccb5eb40a2cb4694c88358e
Ameen Khan <ameenkhan07@gmail.com>
2014-10-08 17:58:47 +0530

    Function to help bill generation | history

--- Includes/admin.php
+++ Includes/admin.php
@@ -7,12 +7,19 @@
         $query .= ", bill.ddate AS ddate, bill.status AS status ";
         $query .= " FROM user , bill ";
         $query .= " WHERE user.id=bill.uid AND aid={$id} ";
-        // echo $query;
         $result = mysqli_query($con,$query);
         if($result === FALSE) {
             die(mysql_error()); // TODO: better error handling
         }
 
+        return $result;
+    }
+
+    function retrieve_bill_data(){
+        include("config.php");
+        $query = "SELECT curdate() as bdate , adddate( curdate(),INTERVAL 30 DAY ) as ddate , user.id AS uid , user.name AS uname FROM user";
+        // echo $query;
+        $result = mysqli_query($con,$query);
         return $result;
     }
  ?>
