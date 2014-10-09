6f2ad54ee662f994c03789360e101834d7df1dd7
Ameen Khan <ameenkhan07@gmail.com>
2014-10-09 19:25:22 +0530

    footer problem solved

--- user/footer.php
+++ user/footer.php
@@ -1,7 +1,9 @@
-<div class="container footer">
-<hr>
-    <a href="#menu-toggle" class="btn btn-primary toggler" id="menu-toggle" style="z-index:1000;position:relative;left:30px;bottom:10px"><i class="icon-circle-arrow-left"></i>Toggle DASH</a>
-    <p  style="height:50px ; padding-left:190px ;margin-top:-40px;text-align:center;bottom: -20px;width:100%;">Created by <a href="https://github.com/ameenkhan07">Ameen M Khan</a> & Abhishek Bhatnagar</p>
-
-</div>
-<!-- <a href="#" class="btn btn-warning"><i class="icon-circle-arrow-left"></i> Toggle</a>  -->
+<!-- FOOTER -->
+    <a href="#menu-toggle" class="btn btn-primary toggler" id="menu-toggle" style="z-index:1000;position:absolute;left:30px;bottom:0px"><i class="icon-circle-arrow-left"></i>Toggle DASH</a>
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                Created by <a href="https://github.com/ameenkhan07">Ameen M Khan</a> & Abhishek Bhatnagar
+            </div>
+        </div>
+    </footer>
